e2fadde9883139968dd43ce10d1b13c3b8e44184
HoaiNam <dangngochoainam0601@gmail.com>
2023-02-07 19:00:57 +0700

    merge develop

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const { dataRouter } = require('./data.router')
 const { sellerRouter } = require('./seller.router')
 const { userRouter } = require('./user.router')
+const { sellerRouter } = require('./seller.router');
+const { userRouter } = require('./user.router');
+const productRouter = require('./product.router');
 
-const indexRouter = require('express').Router()
+const indexRouter = require('express').Router();
 
 indexRouter.use("/user", userRouter)
 indexRouter.use("/seller", sellerRouter)
 indexRouter.use("/data", dataRouter)
+indexRouter.use('/user', userRouter);
+indexRouter.use('/seller', sellerRouter);
+indexRouter.use('/product', productRouter);
 
-module.exports = {indexRouter}
+module.exports = { indexRouter };
