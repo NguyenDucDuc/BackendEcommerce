ac5db3e54caec8a879751dd4a1bfb3182a308471
NguyenDucDuc <nguyenducduc26797@gmail.com>
2023-02-08 10:16:56 +0700

    update feature shop

--- middlewares/shop.middleware.js
+++ middlewares/shop.middleware.js
@@ -1,5 +1,5 @@
 const responseUtil = require("../utils/response.util")
-const {Seller} = require('../models')
+const {Seller, Admin} = require('../models')
 
 module.exports = {
     verifyCreate: async (req, res, next) => {
@@ -13,6 +13,24 @@ module.exports = {
                     status: 403,
                     data: [],
                     errors: "Fobidden"
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            return responseUtil.serverError()
+        }
+    },
+    verifyBlock: async (req, res, next) => {
+        try {
+            const userId = req.data.userId
+            const admin = await Admin.findOne({where: {userId: userId}})
+            if(admin){
+                next()
+            }else {
+                res.status(403).json({
+                    status: 403,
+                    data: [],
+                    errors: "Forbidden"
                 })
             }
         } catch (error) {
