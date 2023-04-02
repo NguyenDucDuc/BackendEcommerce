94f7bd489aa22c533625b22b292b4ebb5e5f142a
Hoai Nam <dangngochoainam0601@gmail.com>
2023-04-03 00:05:25 +0700

    add api getUserByShopId

--- routes/shop.router.js
+++ routes/shop.router.js
@@ -1,19 +1,19 @@
-const shopController = require('../controllers/shop.controller');
-const shopMiddleware = require('../middlewares/shop.middleware');
-const userMiddleware = require('../middlewares/user.middleware');
-const auth = require('../middlewares/auth.middleware');
-const { check } = require('express-validator');
+const shopController = require("../controllers/shop.controller");
+const shopMiddleware = require("../middlewares/shop.middleware");
+const userMiddleware = require("../middlewares/user.middleware");
+const auth = require("../middlewares/auth.middleware");
+const { check } = require("express-validator");
 
-const shopRouter = require('express').Router();
+const shopRouter = require("express").Router();
 
-shopRouter.get("/:id", shopController.getById)
+shopRouter.get("/:id", shopController.getById);
 
 shopRouter.post(
-  '/',
+  "/",
   [
-    check('shopName').notEmpty().withMessage('shop name is required'),
-    check('desc').notEmpty().withMessage('description is required'),
-    check('image').notEmpty().withMessage('image is required'),
+    check("shopName").notEmpty().withMessage("shop name is required"),
+    check("desc").notEmpty().withMessage("description is required"),
+    check("image").notEmpty().withMessage("image is required"),
   ],
   userMiddleware.verifyToken,
   shopMiddleware.verifyCreate,
@@ -21,37 +21,39 @@ shopRouter.post(
 );
 //
 shopRouter.patch(
-  '/block/:id',
+  "/block/:id",
   userMiddleware.verifyToken,
   userMiddleware.verifyAdmin,
   shopController.block
 );
 //
 shopRouter.patch(
-  '/unlock/:id',
+  "/unlock/:id",
   userMiddleware.verifyToken,
   userMiddleware.verifyAdmin,
   shopController.unLock
 );
 //
 shopRouter.get(
-  '/',
+  "/",
   userMiddleware.verifyToken,
   userMiddleware.verifyAdmin,
   shopController.getAll
 );
 //
 shopRouter.delete(
-  '/:shopId',
+  "/:shopId",
   userMiddleware.verifyToken,
   shopMiddleware.verifyDelete,
   shopController.delete
 );
 
 shopRouter.post(
-  '/:shopId/stats',
+  "/:shopId/stats",
   auth.verifyStatsShop,
   shopController.revenueStats
 );
+
+shopRouter.get("/:shopId/get-user", shopController.getUserByShopId);
 
 module.exports = { shopRouter };
